92e46bc17b985ff5db1fe165b1548c3aed9fd7dd
leandroslalves <leandro.usl@live.com>
2025-01-12 00:30:13 -0300

    Estudos de estrutura de dados e algoritmos com C#

---
+++ EstudosDeAlgoritmos/InsertAtTheEnd.cs
@@ -0,0 +1,19 @@
+﻿namespace EstudosDeAlgoritmos
+{
+    public class InsertAtTheEnd
+    {
+        public int[] Add(int[] array, int value)
+        {
+            int[] tempArray = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            tempArray[tempArray.Length - 1] = value;
+
+            return tempArray;
+        }
+    }
+}
---
+++ EstudosDeAlgoritmos/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using EstudosDeAlgoritmos;
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var insertAtTheEnd = new InsertAtTheEnd();
+
+var result = insertAtTheEnd.Add(array, 88);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
+
